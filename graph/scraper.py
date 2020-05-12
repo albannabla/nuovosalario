8358aa8fe3323e45f2085bfab5036a7644591d1b
Alban Nabla <Alban@TKOPlaza.io>
2020-05-12 16:25:31 +0800

    optimized scraper

--- graph/scraper.py
+++ graph/scraper.py
@@ -9,7 +9,7 @@ from .models import Shop
 
 def scraper():
 	# set current working directory
-	os.chdir("/Users/canzonettaclaudio/Documents/CODING/NuovoSalario/ns_root/")
+	# os.chdir("/Users/canzonettaclaudio/Documents/CODING/NuovoSalario/ns_root/")
 
 	# determine number of pages
 	page = requests.get('https://www.immobiliare.it/Roma/vendita_immobili_commerciali/negozio_locale-Roma.html?criterio=rilevanza&pag=1&idMZona[]=10161')
@@ -70,9 +70,21 @@ def scraper():
 	test_df['price'] = test_df['price'].str.replace('€', '')
 	test_df['price'] = test_df['price'].str.replace(' ', '')
 	test_df['price'] = test_df['price'].str.replace('\n', '')
+	test_df['area'] = test_df['area'].astype(str)
 	test_df['area'] = test_df['area'].str.replace('.', '')
-
-
+	test_df.dropna(subset = ['price'], inplace = True)
+	test_df.dropna(subset = ['area'], inplace = True)
+	test_df = test_df[pd.to_numeric(test_df['price'], errors='coerce').notnull()]
+	test_df = test_df[pd.to_numeric(test_df['area'], errors='coerce').notnull()]
+	test_df['area'] = pd.to_numeric(test_df['area'], downcast = 'float')
+	test_df['price'] = pd.to_numeric(test_df['price'], downcast = 'float')
+	test_df = test_df.drop(test_df[test_df.area < 20].index)
+	test_df = test_df.drop(test_df[test_df.price < 50000].index)
+	test_df = test_df.drop(test_df[test_df.price > 300000].index)
+	test_df['pricepersqm'] = test_df.apply(lambda row: row.price / row.area, axis = 1)
+	test_df = test_df.round(2)
+	test_df = test_df.drop(test_df[test_df.pricepersqm < 1000].index)
+	
 	#export in excel 
 	#x = datetime.datetime.now()
 	#oggi =str(x.year)+str(x.month) +str(x.day)
@@ -83,10 +95,12 @@ def scraper():
 
 	#save to database
 	today = date.today()
-	for i in range(len(test_df['id'])):
+	#for i in range(len(test_df['id'])):
 	    #newinstance = Shop.objects.create(webid="test",link="test3",price="test",description="test",area="test",pricepersqm=0,date="2020-05-05")
-	    newinstance = Shop.objects.create(webid=str(test_df['id'][i]),link=str(test_df['link'][i]),price=str(test_df['price'][i]),description=str(test_df['description'][i]),area=str(test_df['area'][i]),pricepersqm=0,date=today)
-	
+	    #newinstance = Shop.objects.create(webid=str(test_df['id'][i]),link=str(test_df['link'][i]),price=str(test_df['price'][i]),description=str(test_df['description'][i]),area=str(test_df['area'][i]),pricepersqm=test_df['pricepersqm'][i],date=today)
+	for row in test_df.iterrows():
+		Shop.objects.create(webid=str(row[1][0]),link=str(row[1][1]),price=str(row[1][2]),description=str(row[1][3]),area=str(row[1][4]),pricepersqm=str(row[1][5]),date=today)
+
 	return
 
 
--- graph/script.py
+++ graph/script.py
@@ -11,19 +11,20 @@ def is_number(s):
 def purge():
 	for webid in Shop.objects.values_list('webid', flat=True).distinct():
 		Shop.objects.filter(pk__in=Shop.objects.filter(webid=webid).values_list('id', flat=True)[1:]).delete()
-	for row in Shop.objects.all():
-		if is_number(row.price) == False or is_number(row.area) == False:
-			row.delete()
-		elif float(row.area) < 20 or float(row.price) < 50000 or float(row.price) > 300000:
-			row.delete()
-	for row in Shop.objects.all():
-		row.pricepersqm = float(row.price)/float(row.area)
-		row.save()
-		if float(row.pricepersqm) < 1000:
-			row.delete()
-	for row in Shop.objects.filter(Q(description__contains="ttivit") | Q(description__contains="essione")):
+	#for row in Shop.objects.all():
+	#	if is_number(row.price) == False or is_number(row.area) == False:
+	#		row.delete()
+	#	elif float(row.area) < 20 or float(row.price) < 50000 or float(row.price) > 300000:
+	#		row.delete()
+	#for row in Shop.objects.all():
+	#	row.pricepersqm = float(row.price)/float(row.area)
+	#	row.save()
+	#	if float(row.pricepersqm) < 1000:
+	#		row.delete()
+	for row in Shop.objects.filter(Q(description__contains="ttivit")):
 		row.delete()
-
+	#for row in Shop.objects.filter(Q(description__contains="essione")):
+	#	row.delete()
 
 
 
