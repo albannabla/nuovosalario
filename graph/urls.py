ef187b31cf1b642adaf0985dea5e4ae15da82b5d
Alban Nabla <Alban@TKOPlaza.io>
2020-05-13 16:30:00 +0800

    added graph in plotly

--- graph/urls.py
+++ graph/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import ShopList, avg
+from .views import ShopList, avg, Graph
 
 urlpatterns = [
     path('', ShopList.as_view()),
     path('average', avg.as_view()),
+    path('graph', Graph.as_view()),
     path('update', views.index),
 ]
--- graph/views.py
+++ graph/views.py
@@ -5,7 +5,8 @@ from .script import purge
 from .scraper import scraper
 import datetime
 from django.db.models import Avg, Func
-
+import plotly.graph_objects as go
+import plotly.offline as opy
 
 class ShopList(ListView):
 	model = Shop
@@ -27,4 +28,24 @@ class avg(TemplateView):
 
 	def shops(self):
 		return Shop.objects.values('date').annotate(averageprice = Round(Avg('pricepersqm')))
+
+class Graph(TemplateView):
+	template_name = 'graph.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(Graph, self).get_context_data(**kwargs)
+		data = Shop.objects.values('date').annotate(averageprice = Round(Avg('pricepersqm')))
+		x = []
+		y = []
+		for i in range(len(data)): 
+			x.append(data[i]['date'])
+			y.append(data[i]['averageprice'])
+		trace1 = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104},mode="lines",  name='1st Trace')
+		data=go.Data([trace1])
+		layout=go.Layout(title="Average Price Per Sqm", xaxis={'title':'date'}, yaxis={'title':'price per sqm (euro)'})
+		figure=go.Figure(data=data,layout=layout)
+		div = opy.plot(figure, auto_open=False, output_type='div')
+		context['graph'] = div
+		return context
+
 
