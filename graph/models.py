9aceccb02dfe4231cc67f59ac562c2e8b3b4b43b
Alban Nabla <Alban@TKOPlaza.io>
2020-05-19 16:13:50 +0800

    modified model fields max lengths

--- graph/models.py
+++ graph/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from datetime import date
 
 class Shop(models.Model):
-	webid = models.CharField(max_length=30)
+	webid = models.CharField(max_length=35)
 	link = models.URLField(max_length=128, blank=True)
-	price = models.CharField(max_length=30)
+	price = models.CharField(max_length=38)
 	description = models.CharField(max_length=100)
-	area = models.CharField(max_length=30)
+	area = models.CharField(max_length=32)
 	pricepersqm = models.FloatField(max_length=30)
 	date = models.DateField(default=date.today)
 
