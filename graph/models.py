fda66c87d5c5c495a726b3625d1839f4b4896e0d
Alban Nabla <Alban@TKOPlaza.io>
2020-05-19 16:56:13 +0800

    modified model fields max lengths

--- graph/models.py
+++ graph/models.py
@@ -4,7 +4,7 @@ from datetime import date
 class Shop(models.Model):
 	webid = models.CharField(max_length=35)
 	link = models.URLField(max_length=128, blank=True)
-	price = models.CharField(max_length=38)
+	price = models.CharField(max_length=95)
 	description = models.CharField(max_length=100)
 	area = models.CharField(max_length=32)
 	pricepersqm = models.FloatField(max_length=40)
